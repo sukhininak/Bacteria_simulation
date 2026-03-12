e2a3556d001c05fba1941211ac78a38802896dad
Polinazavr <70792557+Polinazavr@users.noreply.github.com>
2026-03-12 09:18:22 +0700

    initial simulator

---
+++ simulator.cpp
@@ -0,0 +1,22 @@
+#include "simulator.h"
+
+
+Command::Command(){
+
+};
+
+Move::Move(){
+
+};
+
+bool Move::Exec(Context& ctx){
+
+};// обновляет поле
+
+Look::Look(){
+
+};
+
+bool Look::Exec(Context& ctx){
+    
+};// обновляет поле
---
+++ simulator.h
@@ -0,0 +1,29 @@
+#pragma once
+// #include "src/bacteria.h"
+
+struct Context {
+//  Field* field;
+//  Bacteria* bacteria;//state 
+};
+
+class VirtualMachine{
+public:
+    VirtualMachine();
+    void Start();
+};
+
+class Command{
+public:
+    Command();
+    virtual bool Exec(Context& ctx) = 0; // returns true if command is a terminal
+};
+
+class Move: public Command {
+    Move();
+    bool Exec(Context& ctx) override;// обновляет поле
+};
+
+class Look: public Command {
+    Look();
+    bool Exec(Context& ctx) override;// обновляет поле
+};
